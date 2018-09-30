e027b02312d9c845ec8016d9158ed815521883e7
Dimitri Lozeve <dimitri@lozeve.com>
2018-09-30 14:21:28 +0200

    Barabasi-Albert random model

--- random.rkt
+++ random.rkt
@@ -1,15 +1,31 @@
 #lang racket/base
 
-(require graph)
+(require graph
+	 math/distributions)
 
-(provide erdos-renyi)
+(provide erdos-renyi
+	 barabasi-albert)
 
 (define (erdos-renyi n p)
-  (let ([g (undirected-graph null)])
-    (for ([i (in-range n)])
-      (add-vertex! g i))
-    (for* ([i (in-range n)]
-	   [j (in-range n)])
-      (when (< (random) p) (add-edge! g i j)))
-    g))
+  (define g (undirected-graph null))
+  (for ([i (in-range n)])
+    (add-vertex! g i))
+  (for* ([i (in-range n)]
+	 [j (in-range n)])
+    (when (< (random) p) (add-edge! g i j)))
+  g)
 
+(define (degree g v)
+  (length (get-neighbors g v)))
+
+(define (barabasi-albert n m)
+  (define g (undirected-graph
+	     (for*/list ([i (in-range m)]
+			 [j (in-range m)])
+	       (list i j))))
+  (for ([i (in-range m n)])
+    (let* ([degrees (for/list ([v (in-vertices g)]) (degree g v))]
+	   [neighbors (sample (discrete-dist (get-vertices g) degrees) m)])
+      (for ([n neighbors])
+	(add-edge! g i n))))
+  g)
